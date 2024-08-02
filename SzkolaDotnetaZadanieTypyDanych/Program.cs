6a886b528701d16c382f7bb682eca9a95fb4c3a8
mslowin <slowinski.michal@outlook.com>
2024-08-02 15:42:56 +0200

    Add project files.

---
+++ SzkolaDotnetaZadanieTypyDanych/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SzkolaDotnetaZadanieTypyDanych 
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			// Zadanie 1:
+			string name;
+			string lastName;
+			int age;
+			char sex;
+			string pesel;
+			string workNumber;
+
+			// Zadanie 2:
+			var letter1 = "a";
+			var letter2 = "b";
+			var letter3 = "c";
+
+			Console.WriteLine(letter3 + letter2 + letter1);
+
+			// Zadanie 3:
+
+		}
+	}
+}
